5336cceab50e2438a6cbaa9bad7f3220880d70f8
Serg <kitheus09@gmail.com>
2024-08-10 18:48:14 +0300

    faq droplist animation

--- src/components/FAQ/Droplist.tsx
+++ src/components/FAQ/Droplist.tsx
@@ -1,14 +1,14 @@
-import {useState } from "react";
+import { useState } from "react";
 import styles from "./Droplist.module.css";
 
 import vector24 from "../../assets/svg/FAQ/vector-24-stroke.svg";
 
-export interface Droplistbase{
+export interface Droplistbase {
   question?: string;
   answer?: string;
 }
 
-const Droplist: React.FC<Droplistbase> = ({question, answer}) => {
+const Droplist: React.FC<Droplistbase> = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleAccordion = () => {
@@ -19,16 +19,18 @@ const Droplist: React.FC<Droplistbase> = ({question, answer}) => {
     <div className={styles.frameGroup}>
       <div className={styles.whatIsVeridionParent} onClick={toggleAccordion}>
         <div className={styles.whatIsVeridion}>{question}</div>
-        <img loading="lazy" alt="" src={vector24}/>
+        <img 
+          className={`${styles.arrow} ${isOpen ? styles.rotated : ""}`} 
+          loading="lazy" 
+          alt="" 
+          src={vector24} 
+        />
       </div>
-      {isOpen && (
-        <div className={styles.veridionIsA}>
-          {answer}
-        </div>
-      )}
+      <div className={`${styles.veridionIsA} ${isOpen ? styles.visible : ""}`}>
+        {answer}
+      </div>
     </div>
   );
 };
 
 export default Droplist;
-
