67393416bc6998f0706f2520400b4914afc4c259
Danil <danilskripka1996@gmail.com>
2024-09-02 18:18:40 +0300

    cardsCRMAddElement

--- src/components/CRM/CardsCRM/DigitalCard.tsx
+++ src/components/CRM/CardsCRM/DigitalCard.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../CRM/CardsCRM/DigitalCard.module.css';
 import arrow from "../../../assets/svg/CRM/Dashboard/arrow.svg";
-import bacl from "../../../assets/svg/CRM/CardsCRM/Digital_Card_plan.svg"
-import rectSt from "../../../assets/svg/CRM/CardsCRM/Rectangle 145.svg"
-import St from "../../../assets/svg/CRM/CardsCRM/Vector1.svg"
-import card11 from "../../../assets/svg/CRM/CardsCRM/Card_01.svg"
-import card12 from "../../../assets/svg/CRM/CardsCRM/Card_02.svg"
-import card13 from "../../../assets/svg/CRM/CardsCRM/Card_03.svg"
+import bacl from "../../../assets/svg/CRM/CardsCRM/Digital_Card_plan.svg";
+import rectSt from "../../../assets/svg/CRM/CardsCRM/Rectangle 145.svg";
+import St from "../../../assets/svg/CRM/CardsCRM/Vector1.svg";
+import card11 from "../../../assets/svg/CRM/CardsCRM/Card_01.svg";
+import card12 from "../../../assets/svg/CRM/CardsCRM/Card_02.svg";
+import card13 from "../../../assets/svg/CRM/CardsCRM/Card_03.svg";
+
+import card1R from "../../../assets/svg/CRM/CardsCRM/Property1.png";
+import card2R from "../../../assets/svg/CRM/CardsCRM/Property2.png";
+import card3R from "../../../assets/svg/CRM/CardsCRM/Property3.png";
+
 interface DigitalCardContainerProps {
   DigitalCard: string;
   buttonFree: string;
@@ -17,7 +22,23 @@ const DigitalCard: React.FC<DigitalCardContainerProps> = ({
     DigitalCard,
     buttonFree,
     TextDigital
+    
 }) => {
+  const [currentImage1, setCurrentImage1] = useState(card11);
+  const [currentImage2, setCurrentImage2] = useState(card12);
+  const [currentImage3, setCurrentImage3] = useState(card13);
+
+  const toggleImage1 = () => {
+    setCurrentImage1(prevImage => (prevImage === card11 ? card3R : card11));
+  };
+
+  const toggleImage2 = () => {
+    setCurrentImage2(prevImage => (prevImage === card12 ? card2R : card12));
+  };
+
+  const toggleImage3 = () => {
+    setCurrentImage3(prevImage => (prevImage === card13 ? card1R : card13));
+  };
   return (
     <div className={styles.DigitalCardContainer}
     style={{ backgroundImage: `url(${bacl})` }}
@@ -42,10 +63,22 @@ const DigitalCard: React.FC<DigitalCardContainerProps> = ({
             <img className={styles.absRect2} src={St}></img>
             
         </div>
-        <img src={card11}></img>
-            <img src={card12}></img>
-            <img src={card13}></img>
-        
+          <div>
+            <img className={styles.rasmerImg} src={currentImage1} />
+              <button className={styles.btn1Card} onClick={toggleImage1}>
+             </button>
+            </div>
+         
+           
+            <div>
+              <img className={styles.rasmerImg} src={currentImage2}></img>
+              <button className={styles.btn1Card} onClick={toggleImage2}></button>
+            </div>
+            <div>
+              <img className={styles.rasmerImg} src={currentImage3}></img>
+              <button className={styles.btn1Card} onClick={toggleImage3}></button>
+            </div>
+            
     </div>
 
     </div>
--- src/pages/CRM/CardsCRM.tsx
+++ src/pages/CRM/CardsCRM.tsx
@@ -8,6 +8,8 @@ import Menu_mini from "../../components/CRM/Menu_mini";
 import styles from "../../pages/CRM/CardsCRM.module.css";
 import DigitalCards from "../../components/CRM/CardsCRM/DigitalCard"
 import FreeButton from "../../assets/svg/CRM/CardsCRM/Component 9.svg"
+import CardsGirl from "../../components/CRM/CardsCRM/CardsGirl";
+import WhatsNews from "../../components/CRM/CardsCRM/WhatsNews";
 
 
 const CardsCRM: FunctionComponent = () => {
@@ -18,11 +20,17 @@ const CardsCRM: FunctionComponent = () => {
         <div className={styles.styleColumn}>
         <div className={styles.styleCardWord}>Cards</div>
         <DigitalCards
-             DigitalCard="Digital Card"
+             DigitalCard="Digital card"
              buttonFree="Free"
              TextDigital="A card that exists only online, but works everywhere. Easy, safe and always at hand."
 
         ></DigitalCards>
+        <div className={styles.styleROWs}>
+            <CardsGirl></CardsGirl>
+            <WhatsNews></WhatsNews>
+            <WhatsNews></WhatsNews>
+        </div>
+        
         </div>
         </main>
         
