b41d3c87bfb8caf33d06ca38c2a91b0988bd0930
Danil <danilskripka1996@gmail.com>
2024-09-05 17:07:29 +0300

    SubAddComponents50%

--- src/components/CRM/SubscriptionsCRM/Sub.tsx
+++ src/components/CRM/SubscriptionsCRM/Sub.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../CRM/SubscriptionsCRM/Sub.module.css';
 import CashBackImg from "../../../assets/svg/CRM/CardsCRM/Cashback.svg";
 
-
+import line1 from "../../../assets/svg/CRM/CardsCRM/Group 104.svg";
+import line2 from "../../../assets/svg/CRM/CardsCRM/Group 1044.svg";
 
 interface SubscriptionsForROWContainerProps {
         DepositsIMG: string;
@@ -16,13 +17,29 @@ const SubscriptionsForROW: React.FC<SubscriptionsForROWContainerProps> = ({
         DepositsBalance
           
 }) => {
+    const [currentImage, setCurrentImage] = useState(line1);
+
+    const handleMouseEnter = () => {
+      setCurrentImage(line2);
+    };
+  
+    const handleMouseLeave = () => {
+      setCurrentImage(line1);
+    };
   return (
     <div className={styles.Container}>
+        <div className={styles.Column}>
+        <button className={styles.Btn}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+    
+          <img src={currentImage} />
+        </button>
         <div className={styles.stIMG}>
             <img  src={DepositsIMG}>
             </img>
         </div>
-        <div className={styles.Column}>
+        
             <div className={styles.stTXT}>
             {DepositsTXT}
             </div>
